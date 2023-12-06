8018fff8eaa8c56c0bbff1e32c87c1bb536be798
EliaBuratti <elia.buratti.dev@gmail.com>
2023-12-06 10:09:37 +0100

    add images and thumb path

--- config/db_apartments.php
+++ config/db_apartments.php
@@ -8,19 +8,16 @@ return [
             "city" => "Rota D'imagna, Lombardia",
             "postal_code" => '24037',
             "address" => "Via V. Emanuele 30",
-            /*         "latitude" =>
-            "longitude" => */
             "rooms" => 4,
             "bathrooms" => 2,
             "beds" => 6,
             "m_square" => 220,
-            "description" => "Informazioni su questo spazio
-            Rilassati con tutta la famiglia in questo alloggio tranquillo, immerso nel verde della Valle Imagna.
+            "description" => "Rilassati con tutta la famiglia in questo alloggio tranquillo, immerso nel verde della Valle Imagna.
             Casa completa di cucina, bagni, doccia, divano, tv, wifi, tavoli esterni, spazio per barbecue.
             Possibilità di passeggiate nel verde a piedi o in bicicletta, vicinanza ad aziende agricole, vicinanza a spa, ristoranti e pizzerie.
             Gestito da un'azienda agricola, al Vostro arrivo troverete un cestino di benvenuto con i loro prodotti.",
-            "thumbnail" =>'',
-            "visible" => false,
+            "thumbnail" =>"apartments/1app/3b62955a-2a58-4bf4-9aed-13c455feea84.png",
+            "visible" => true,
         ],
         [
             "title" => "Intero alloggio: unità in affitto",
@@ -28,8 +25,6 @@ return [
             "city" => "Como, Lombardia",
             "postal_code" => '22100',
             "address" => "Piazza Alcide de Gasperi 2",
-              /*         "latitude" =>
-            "longitude" => */
             "rooms" => 2,
             "bathrooms" => 1,
             "beds" => 2,
@@ -38,8 +33,8 @@ return [
             Se c'è un bambino non è un problema sia come un lettino per bambini, sedia e bagno sono disponibili.
             Esci dalla porta e goditi una passeggiata sul lago.
             Parte di un elegante edificio, l'appartamento è situato in una posizione ideale oltre alla riva del Lago di Como. È luminosa e ariosa, ed è composta da un'accogliente sala principale di 30 mq, con un letto matrimoniale e un divano, un comodo bagno, e una cucina completamente separata.",
-            "thumbnail" =>'',
-            "visible" => false,
+            "thumbnail" =>'apartments/2app/01e60145_original.png',
+            "visible" => true,
         ],
         [
             "title" => "Intero alloggio: chalet",
@@ -47,8 +42,6 @@ return [
             "city" => "Cesana torinese, Piemonte",
             "postal_code" => '10054',
             "address" => "Strada del lens 5",
-            /*         "latitude" =>
-            "longitude" => */
             "rooms" => 5,
             "bathrooms" => 3,
             "beds" => 5,
@@ -57,8 +50,8 @@ return [
             Situato all'ingresso del piccolo borgo montano di Fenils è circondato da splendidi boschi e prati fioriti.
             Completamente indipendente con giardino privato, è costeggiato dal suggestivo Riòou d'Finhòou corso d'acqua che sgorga sulle pendici del Monte Chaberton
             A soli 5' degli impianti sciistici della Via Lattea, presenta tutti i comfort necessari per una vacanza perfetta.",
-            "thumbnail" =>'',
-            "visible" => false,
+            "thumbnail" =>'apartments/3app/2fcbf834-6be6-48c2-8594-2019e0132625.png',
+            "visible" => true,
         ],
         [
             "title" => "Appartamento di lusso a Castello Planta Meran",
@@ -66,15 +59,13 @@ return [
             "city" => "Merano, Trentino-Alto Adige",
             "postal_code" => '39012',
             "address" => "Via XXX Aprile 4",
-              /*         "latitude" =>
-            "longitude" => */
             "rooms" => 15,
             "bathrooms" => 8,
             "beds" => 13,
             "m_square" => 240,
             "description" => "Appartamento di lusso a Schloss Planta, ala nord del castello, il cosiddetto 'Beletage'. Chiunque si chieda da dove provenga il nome troverà la risposta guardando fuori dalle finestre. In uno dei posti più belli dell'Alto Adige, ai margini dell'Obermais - Merano raggiungibile a piedi - incastonato tra fiori, prati e meli. è top attrezzato con mobili eleganti e un moderno bagno e cucina, l'uso del giardino è incluso.",
-            "thumbnail" =>'',
-            "visible" => false,
+            "thumbnail" =>'apartments/4app/13e0a068-ba63-4a7e-b420-d66fd7932fee.png',
+            "visible" => true,
         ],
         [
             "title" => "Villa Niobe | Appartamento esclusivo con spiaggia privata",
@@ -82,8 +73,6 @@ return [
             "city" => "Stresa, Piemonte",
             "postal_code" => '28838',
             "address" => "Corso Umberto I 73",
-               /*         "latitude" =>
-            "longitude" => */
             "rooms" => 3,
             "bathrooms" => 3,
             "beds" => 7,
@@ -91,8 +80,8 @@ return [
             "description" => "Situato accanto al famoso Parco del Pallavicino, a 1 km dal centro di Stresa, al primo piano di un'incantevole e storica villa liberty recentemente rinnovata, con un rigoglioso giardino che circonda il retro della proprietà, l'appartamento gode di un'eccezionale vista frontale sul lago e di un accesso privato ad una bellissima spiaggia attrezzata.
             L'appartamento è elegante, arioso e molto luminoso grazie alle ampie finestre e balconi presenti in ogni stanza. Ha una superficie di 150 mq e dotato di 2 incantevoli camere matrimoniali e 1 camera tripla, molto confortevoli per un massimo di 7 persone, 3 bagni completi, una meravigliosa zona giorno con una splendida terrazza affacciata sul lago, tavolo da pranzo all'aperto e al coperto, 2 divani, Smart TV e una cucina completamente attrezzata.
             Proprio di fronte alla proprietà, attraversando la strada principale che costeggia il lungolago, si accede alla meravigliosa spiaggia privata, attrezzata con sdraio, ombrelloni e doccia.",
-            "thumbnail" =>'',
-            "visible" => false,
+            "thumbnail" =>'apartments/5app/2a57fef9-e34c-4359-bc93-b6bedf84a35a.png',
+            "visible" => true,
         ],
         [
             "title" => "Iurta con bagno, riscaldata",
@@ -100,8 +89,6 @@ return [
             "city" => "Ostana, Piemonte",
             "postal_code" => '12030',
             "address" => "Via Roma 54",
-                /*         "latitude" =>
-            "longitude" => */
             "rooms" => 1,
             "bathrooms" => 1,
             "beds" => 2,
@@ -111,8 +98,8 @@ return [
             Vista mozzafiato sul Monviso e sui nostri Alpaca e le nostre Capre Cashmere che pascolano a pochi metri dalle yurte.
             La colazione te la portiamo in un grande cesto.
             Il tutto completato dalla tinozza finlandese riscaldata con stufa (a pagamento). Possibilità di mangiare nel nostro agriturismo di famiglia",
-            "thumbnail" =>'',
-            "visible" => false,
+            "thumbnail" =>'apartments/6app/5c769339-f134-4d0a-8588-1a4186cef295.png',
+            "visible" => true,
         ],
         [
             "title" => "Campino, Italia. Intero alloggio: villa",
@@ -120,8 +107,6 @@ return [
             "city" => "Campino, Piemonte",
             "postal_code" => '28838',
             "address" => "Via La Riana 6",
-             /*         "latitude" =>
-            "longitude" => */
             "rooms" => 5,
             "bathrooms" => 4,
             "beds" => 8,
@@ -130,8 +115,8 @@ return [
             - Il piano terra è composto da un'ampia zona giorno con finestre a tutta altezza per godere dell'incredibile vista sul lago, una zona pranzo, un angolo salotto con comodi divani e TV e un camino aperto. La cucina, dotata di elettrodomestici di alta qualità, dà accesso al tavolo esterno e al giardino. Servizi igienici. Nella zona notte si trovano una camera da letto principale con letto matrimoniale e bagno privato con vasca, e una seconda camera da letto con letto matrimoniale e bagno privato con doccia.
             - Attraverso l'elegante scala a chiocciola, si raggiunge il primo piano, dove si trova un'accogliente zona soggiorno con divani (trasformabili in due letti singoli), TV e scrivania. La zona notte su questo piano è composta da una camera con letto matrimoniale, una camera con due letti singoli e un bagno con vasca idromassaggio e doccia.
             - Al piano inferiore troverete la vostra area spa privata con una mini piscina con massaggio ad aria, una doccia emozionale e un'area relax con una selezione di tisane. Accesso alla zona piscina esterna.",
-            "thumbnail" =>'',
-            "visible" => false,
+            "thumbnail" => 'apartments/7app/8cf1047f-48d8-41b0-807c-095495fe9854.png',
+            "visible" => true,
         ],
         [
             "title" => "La Stalla - Casa San Gabriel",
@@ -139,8 +124,6 @@ return [
             "city" => "Pierantonio, Umbria",
             "postal_code" =>  '06019',
             "address" => "Via Palmiro Togliatti 51",
-             /*         "latitude" =>
-            "longitude" => */
             "rooms" => 1,
             "bathrooms" => 1,
             "beds" => 2,
@@ -150,8 +133,8 @@ return [
             L'aspetto esposto a sud assicura che l'area sfrutti il sole per tutta la giornata e la sua posizione elevata spesso porta una brezza rinfrescante per un sollievo di benvenuto al caldo dell'estate.
             È il luogo perfetto per gustare un fresco bicchiere di 'Vino Bianco' e guardare il sole della sera tramontare sulle montagne.
             L'intera area è stata recintata per garantire la sicurezza dei bambini, ma in modo tale che questo non pregiudichi la vista mozzafiato dalla piscina.",
-            "thumbnail",
-            "visible" => false,
+            "thumbnail" => 'apartments/8app/2d502704-5db1-47c0-bfee-3816d04743fb.png',
+            "visible" => true,
         ],
         [
             "title" => "Attico Ascarelli flat terrazzo privato metro duomo",
@@ -159,16 +142,59 @@ return [
             "city" => "Napoli, Campania",
             "postal_code" =>  '80133',
             "address" => "Via Duomo 326",
-             /*         "latitude" =>
-            "longitude" => */
             "rooms" => 2,
             "bathrooms" => 2,
             "beds" => 3,
             "m_square" => 55,
             "description" => "Goditi questo meraviglioso attico (65mq) con ampio terrazzo a livello e due camere da letto nel cuore della città. Nuovo, arredato in stile minimalista. Accanto alla stazione metro Duomo e circondato da negozi e ristoranti (a un minuto dalla pizzeria più famosa di Napoli).",
-            "visible" => false,
+            "thumbnail" => 'apartments/9app/1d014c26-77bb-43de-8518-400746c5fae3.png',
+            "visible" => true,
         ],
-
+        [
+            "title" => "Intero alloggio: appartamento",
+            "nation" => "Italy",
+            "city" => "Lecce, Puglia",
+            "postal_code" =>  '73100',
+            "address" => "Via di Valesio 93",
+            "rooms" => 1,
+            "bathrooms" => 1,
+            "beds" => 2,
+            "m_square" => 68,
+            "description" => "La Piccicca è la seconda struttura che gestisco personalmente, appena ristrutturata, e a soli 5 minuti a piedi dal Duomo. Ideale per una coppia, in una antica caratteristica abitazione del centro storico di Lecce, con pregi e difetti, dotata di tutti i confort, di un piccolo ortale retrostante privato e di una piccola sauna ad infrarossi ad uso esclusivo.
+            La sua ottima posizione garantisce l'assenza assoluta di schiamazzi e rumori, in ogni momento del giorno e soprattutto della notte.",
+            "thumbnail" => 'apartments/10app/676ae1ca-4311-4c69-bd24-24e7cb8d4f0a.png',
+            "visible" => true,
+        ],
+        [
+            "title" => "Splendida Casa sull'Albero a Pochi Minuti da Firenze",
+            "nation" => "Italy",
+            "city" => "Firenze, Toscana",
+            "postal_code" =>  '50121',
+            "address" => "Borgo Pinti 99",
+            "rooms" => 1,
+            "bathrooms" => 1,
+            "beds" => 2,
+            "m_square" => 18,
+            "description" => "casaBARTHEL è un luogo perfetto per una vacanza e una residenza d'artista, immerso nel paesaggio toscano a soli venti minuti dal Duomo e ad un'ora da Siena. Vieni a vivere con noi: goditi i nostri ulivi, l'orto, il campo da tennis e la piccola piscina.",
+            "thumbnail" => 'apartments/11app/7115f8b1-f4cd-4ed2-adf9-78aff0011759.png',
+            "visible" => true,
+        ],
+        [
+            "title" => "Crotto Polirolo - The House Of Travelers",
+            "nation" => "Italy",
+            "city" => "Cernobbio, Lombardia",
+            "postal_code" =>  '22012',
+            "address" => "Via Regina 40",
+            "rooms" => 2,
+            "bathrooms" => 2,
+            "beds" => 11,
+            "m_square" => 70,
+            "description" => "Crotto Polirolo è un appartamento ampio ed elegante dotato di una grande terrazza vista lago situato nella zona alta di Cernobbio.
+            La vera perla di questo appartamento è la sua terrazza da cui godere in pieno relax di un panorama mozzafiato o di una cena tra amici.
+            Il nostro appartamento rappresenta il luogo ideale per trascorrere una vacanza tra amici o in famiglia sulle sponde del lago più bello al mondo.",
+            "thumbnail" => 'apartments/12app/0d0ff6e7-8398-4d02-aaa2-5feeff328ad6.png',
+            "visible" => true,
+        ],
     ]
 ]
 
