de528fa46ffe2b931c1547cc0ed6323218cf7073
EliaBuratti <elia.buratti.dev@gmail.com>
2023-12-06 14:18:50 +0100

    add class seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            ApartmentSeeder::class,
+            //ImageSeeder::class,
+            //MessageSeeder::class,
+            //ServiceSeeder::class,
+            //SponsorshipSeeder::class,
+            //ViewSeeder::class,
+        ]);
     }
 }
