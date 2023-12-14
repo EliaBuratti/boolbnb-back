67f323647a17624882d61e5f37869782a4be7e2f
EliaBuratti <elia.buratti.dev@gmail.com>
2023-12-14 14:26:58 +0100

    create statment for apartment sponsorship not finished

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -4,27 +4,63 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Api\PaymentRequest;
+use App\Models\Apartment;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
+
+use function PHPUnit\Framework\throwException;
 
 class PaymentController extends Controller
 {
 
     public function index(Request $request)
     {
+        //dd($request);
 
-    $nonce = $request->nonce;
-    $gateway = $this->brainConfig();
-    $status = $gateway->transaction()->sale([
-	'amount' => '10.00',
-	'paymentMethodNonce' => $nonce,
-	'options' => [
-	    'submitForSettlement' => True
-	]
-    ]);
+        $validatedData = $request->validate([
+            'sponsorship' => ['required', 'numeric', 'exists:sponsorships,id'],
+            'apartmentid' =>['required', 'numeric','exists:apartments,id'],
+        ]);
 
-    dd(response()->json($status));
-    return response()->json($status);
+        $user_id = auth()->user()->id;  
+
+        $apartments = Apartment::where('user_id', '=', $user_id)->get();
+
+        foreach($apartments as $apartment){
+
+            $idAppUser = $apartment->user_id;
+
+            if($idAppUser === $user_id) {
+                dd($apartment);
+                if(true){
+                    $apartment->sponsorships()->attach([$validatedData['sponsorship'],  '2023/12/23 12:13:03']);
+                }
+
+                dd($user_id);    
+                $nonce = $request->nonce;
+                $gateway = $this->brainConfig();
+                $status = $gateway->transaction()->sale([
+                'amount' => '10.00',
+                'paymentMethodNonce' => $nonce,
+                'options' => [
+                    'submitForSettlement' => True
+                    ]
+                ]);
+        
+/*                 if($status->success){
+                    $apartment->sponsorships()->attach([$apartment->id]);
+                } */
+                dd($status->success);// true o false if payment it'ok
+                //dd(response()->json($status));
+                return response()->json($status);
+
+            } else {
+                return redirect('https://www.youtube.com/shorts/pSq2Jgl430s');
+            }
+        }
+
+        
+       
 }
 
 public function brainConfig()
