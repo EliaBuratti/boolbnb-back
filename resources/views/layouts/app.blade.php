0beb2fb6297019317ce9b7341211ffcb0c6ea649
EliaBuratti <elia.buratti.dev@gmail.com>
2023-12-20 17:17:35 +0100

    fix title page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>BoolBnB</title>
 
 
     <!-- Fonts -->
