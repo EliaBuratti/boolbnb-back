cdfaa4e7f1c511c46361562af7e10987083e4e2f
EliaBuratti <elia.buratti.dev@gmail.com>
2023-12-20 11:01:27 +0100

    create file and update seeder messages

---
+++ config/db_messages.php
@@ -0,0 +1,184 @@
+<?php
+return [
+    'messages' => [
+        [
+            'name' => 'Mario Rossi',
+            'message' => 'Salve, sono interessato all\'appartamento che ho visto sul vostro sito. Potrei avere ulteriori informazioni riguardo la disponibilità per il mese di agosto?',
+            'email' => 'mario.rossi@example.com',
+        ],
+        
+        [
+            'name' => 'Luigi Bianchi',
+            'message' => 'Buongiorno, vorrei sapere se l\'appartamento ha il WiFi incluso.',
+            'email' => 'luigi.bianchi@example.com',
+        ],
+        
+        [
+            'name' => 'Francesca Neri',
+            'message' => 'Ciao, ho notato che l\'appartamento ha una bellissima vista sul mare. È disponibile per la settimana di luglio?',
+            'email' => 'francesca.neri@example.com',
+        ],
+        
+        [
+            'name' => 'Giuseppe Verdi',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il weekend del 20 luglio. È disponibile?',
+            'email' => 'giuseppe.verdi@example.com',
+        ],
+        
+        [
+            'name' => 'Anna Bianco',
+            'message' => 'Buongiorno, l\'appartamento ha l\'aria condizionata inclusa?',
+            'email' => 'anna.bianco@example.com',
+        ],
+        
+        [
+            'name' => 'Marco Nero',
+            'message' => 'Ciao, l\'appartamento ha un parcheggio incluso?',
+            'email' => 'marco.nero@example.com',
+        ],
+        
+        [
+            'name' => 'Laura Rosso',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il mese di settembre. È disponibile?',
+            'email' => 'laura.rosso@example.com',
+        ],
+        
+        [
+            'name' => 'Stefano Giallo',
+            'message' => 'Buongiorno, l\'appartamento ha una cucina attrezzata?',
+            'email' => 'stefano.giallo@example.com',
+        ],
+        
+        [
+            'name' => 'Maria Verde',
+            'message' => 'Ciao, l\'appartamento ha una lavatrice?',
+            'email' => 'maria.verde@example.com',
+        ],
+        
+        [
+            'name' => 'Paolo Blu',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il weekend del 15 agosto. È disponibile?',
+            'email' => 'paolo.blu@example.com',
+        ],
+        
+        [
+            'name' => 'Sara Viola',
+            'message' => 'Buongiorno, l\'appartamento ha un balcone?',
+            'email' => 'sara.viola@example.com',
+        ],
+        
+        [
+            'name' => 'Antonio Marrone',
+            'message' => 'Ciao, l\'appartamento ha un giardino?',
+            'email' => 'antonio.marrone@example.com',
+        ],
+        
+        [
+            'name' => 'Giulia Rosa',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il mese di ottobre. È disponibile?',
+            'email' => 'giulia.rosa@example.com',
+        ],
+        
+        [
+            'name' => 'Roberto Arancione',
+            'message' => 'Buongiorno, l\'appartamento ha un servizio di pulizia incluso?',
+            'email' => 'roberto.arancione@example.com',
+        ],
+        
+        [
+            'name' => 'Valentina Grigio',
+            'message' => 'Ciao, l\'appartamento ha un servizio di colazione incluso?',
+            'email' => 'valentina.grigio@example.com',
+        ],
+        [
+            'name' => 'Federico Celeste',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il weekend del 1 novembre. È disponibile?',
+            'email' => 'federico.celeste@example.com',
+        ],
+        
+        [
+            'name' => 'Chiara Turchese',
+            'message' => 'Buongiorno, l\'appartamento ha un servizio di trasporto incluso?',
+            'email' => 'chiara.turchese@example.com',
+        ],
+        
+        [
+            'name' => 'Davide Oro',
+            'message' => 'Ciao, l\'appartamento ha un servizio di lavanderia incluso?',
+            'email' => 'davide.oro@example.com',
+        ],
+        
+        [
+            'name' => 'Elena Argento',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il mese di dicembre. È disponibile?',
+            'email' => 'elena.argento@example.com',
+        ],
+        
+        [
+            'name' => 'Giovanni Bronzo',
+            'message' => 'Buongiorno, l\'appartamento ha un servizio di pulizia giornaliero incluso?',
+            'email' => 'giovanni.bronzo@example.com',
+        ],
+        
+        [
+            'name' => 'Irene Rame',
+            'message' => 'Ciao, l\'appartamento ha un servizio di colazione a letto incluso?',
+            'email' => 'irene.rame@example.com',
+        ],
+        
+        [
+            'name' => 'Leonardo Zinco',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il weekend del 25 dicembre. È disponibile?',
+            'email' => 'leonardo.zinco@example.com',
+        ],
+        
+        [
+            'name' => 'Monica Ferro',
+            'message' => 'Buongiorno, l\'appartamento ha un servizio di navetta aeroportuale incluso?',
+            'email' => 'monica.ferro@example.com',
+        ],
+        
+        [
+            'name' => 'Nicola Stagno',
+            'message' => 'Ciao, l\'appartamento ha un servizio di baby-sitting incluso?',
+            'email' => 'nicola.stagno@example.com',
+        ],
+        
+        [
+            'name' => 'Patrizia Piombo',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il mese di gennaio. È disponibile?',
+            'email' => 'patrizia.piombo@example.com',
+        ],
+        
+        [
+            'name' => 'Riccardo Alluminio',
+            'message' => 'Buongiorno, l\'appartamento ha un servizio di consegna della spesa incluso?',
+            'email' => 'riccardo.alluminio@example.com',
+        ],
+        
+        [
+            'name' => 'Simona Titanio',
+            'message' => 'Ciao, l\'appartamento ha un servizio di noleggio biciclette incluso?',
+            'email' => 'simona.titanio@example.com',
+        ],
+        
+        [
+            'name' => 'Tommaso Magnesio',
+            'message' => 'Salve, vorrei prenotare l\'appartamento per il weekend del 14 febbraio. È disponibile?',
+            'email' => 'tommaso.magnesio@example.com',
+        ],
+        
+        [
+            'name' => 'Valeria Vanadio',
+            'message' => 'Buongiorno, l\'appartamento ha un servizio di noleggio auto incluso?',
+            'email' => 'valeria.vanadio@example.com',
+        ],
+        
+        [
+            'name' => 'Vittorio Cromo',
+            'message' => 'Ciao, l\'appartamento ha un servizio di prenotazione ristoranti incluso?',
+            'email' => 'vittorio.cromo@example.com',
+        ],
+    ]
+]
+?>
--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
+use App\Models\Message;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,19 @@ class MessageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $messages = config('db_messages.messages');
+        $apartments = Apartment::all();
+
+        foreach($messages as $message) {
+
+            $new_message = new Message();
+            $new_message->name = $message['name'];
+            $new_message->message = $message['message'];
+            $new_message->email = $message['email'];
+            $new_message->apartment_id = rand(1, 12);
+            $new_message->subject = 'information about ' . $apartments[$new_message->apartment_id - 1]->title;
+            $new_message->save();
+        }
     }
 }
